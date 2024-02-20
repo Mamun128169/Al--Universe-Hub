68f954b27e083d3342382a94e77f6ad1ef05fcc9
Mamun128169 <mh4559641@gmail.com>
2024-02-21 00:28:38 +0600

    modal part half done

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ const cardsContainer = document.getElementById("cards-container");
 const showAllBtn = document.getElementById("show-all-btn");
 const sortBtn = document.getElementById("sort-btn");
 const loader = document.getElementById("loader");
+const handleModalBtn = document.getElementById("show-modal-btn");
 
 const loadData = async (isShowAll) => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
@@ -37,6 +38,7 @@ const sortByDate = (items) => {
 };
 
 const displayData = (item) => {
+  // console.log(item);
   const cardDiv = document.createElement("div");
   cardDiv.classList = "card bg-base-100 shadow-xl border border-2";
 
@@ -65,7 +67,13 @@ const displayData = (item) => {
     item.published_in
   )}">
     </div>
-    <span class="bg-rose-500 text-white px-2 py-1 rounded-full text-2xl text-center cursor-pointer hover:bg-rose-700">&rarr;</span>
+    <div >
+    <span id="show-modal-btn" 
+     onclick="handleShowModal('${item.id}')" 
+     class="bg-rose-500 text-white px-2 py-1 rounded-full text-2xl text-center cursor-pointer hover:bg-rose-700">
+     &rarr;
+    </span>
+    </div>
  </div>
     `;
 
@@ -104,5 +112,63 @@ const sortedLoadedData = async () => {
 //handle sort button
 sortBtn.addEventListener("click", sortedLoadedData);
 const url = `https://openapi.programming-hero.com/api/ai/tools`;
+
+
+const displayModalData = (data) => {
+  const modalBoxContainer = document.getElementById("modal-box");
+
+  const {description, pricing, features, integrations } = data;
+
+  const featuresArr = Object.values(features);
+  console.log(featuresArr);
+ 
+  modalBoxContainer.innerHTML = `
+    <div class=" mx-3 border border-red-500 p-6 rounded bg-rose-100">
+      <h2 class="text-xl mb-4 font-bold">${description}</h2>
+      <div class="flex items-center justify-center gap-2">
+        ${
+          pricing.map((val) => `<div class="p-3 my-3 cursor-pointer text-center text-sm font-bold text-green-700 bg-slate-100 rounded-lg">
+           <p>${val.price}</p>
+           <p>${val.plan}</P>
+          </div>`).join("")
+        }
+      </div>
+      <div class="flex justify-between">
+        <div class="my-5">
+          <h2 class="text-xl font-bold my-4">Features</h2>
+          <ul class="list-disc text-base text-gray-600 pl-5"> 
+            ${
+              featuresArr.map(val => `<li>${val.feature_name}</li>`).join("")
+            }
+          </ul> 
+        </div>
+        <div class="my-5">
+          <h2 class="text-xl font-bold my-4">Integrations</h2>
+          <ul class="list-disc text-base text-gray-600 pl-5"> 
+            ${
+              integrations.map(val => `<li>
+              ${val}
+              </li>`).join("")
+            }
+          </ul> 
+        </div>
+      </div> 
+    </div>
+  `
+}
+
+const handleShowModal = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  let itemData = data.data;
+  console.log(itemData);
+
+  displayModalData(itemData);
+  
+  // call to open up modal
+  AI_Tool_Modal.showModal();
+}
 
 
